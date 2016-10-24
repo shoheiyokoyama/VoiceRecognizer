d89c68c556cfba77817a585895993bc73567fec0
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-10-24 16:40:47 +0900

    fixed

--- VoiceRecognizer/ViewController.swift
+++ VoiceRecognizer/ViewController.swift
@@ -14,17 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet fileprivate weak var titileLabel: UILabel!
     
     @IBOutlet private weak var button: UIButton!
-    private let recognizer: VoiceRecognizer = .init(sourceNames: ["v1.m4a", "v2.m4a", "v3.m4a", "v4.m4a", "v5.m4a"], taskName: "task")
+    private var recognizer: VoiceRecognizer?
 
     // MARK: - Life Cycle Methods -
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        recognizer.delegate = self
+        
+        let sourceFiles = ["movie1.m4a", "movie2.m4a", "movie3.m4a"]
+        
+        // example
+        
+//        let filename = "サントリー動画"
+//        let sourceFiles = (0..<24).enumerated().map { index in
+//            return filename + "-" + "\(index.element + 1)" + ".m4a"
+//        }
+        
+        recognizer = VoiceRecognizer(sourceNames: sourceFiles, taskName: "サントリー")
+        recognizer?.delegate = self
     }
     
     @IBAction func tappedButton(_ sender: AnyObject) {
-        recognizer.startRecordining()
+        recognizer?.startRecordining()
         button.isEnabled = false
     }
 }
